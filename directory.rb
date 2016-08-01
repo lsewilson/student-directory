82c61dbcb78a9786c75bf9a2a2755b51930b2a26
lsewilson <laurawilson@hotmail.co.uk>
2016-08-01 12:50:12 +0100

    Created student array and removed repetition

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
-# first we print the list of students
+# let's put all students into an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator"
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+# and then print them
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# finally, we print the total number of studnets
-puts "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end 
+# finally, we print the total
+puts "Overall, we have #{students.count} great students"
