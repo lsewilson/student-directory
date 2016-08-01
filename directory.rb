ef23ed92efb54fa1e46da4eb0298516d9a54f3af
lsewilson <laurawilson@hotmail.co.uk>
2016-08-01 14:46:00 +0100

    Added specific letter function

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students."
+  puts "To finish, just hit return twice."
   #create an empty array
   students = []
   # get the first name
@@ -8,12 +8,18 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     students << {name: name, cohort: :August}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students."
     # get another name from the user
     name = gets.chomp
   end
   # return the array of students
   students
+end
+
+def certain_letter
+  puts "With which letter does the names you wish to search for begin?"
+  puts "To search for all names, just hit return twice."
+  @initial = gets.chomp.upcase
 end
 
 def print_header
@@ -23,15 +29,21 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name][0] == @initial
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    elsif @initial == "" || @initial == nil?
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
+
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students."
 end
 # nothing happens until we call the methods
 students = input_students
+certain_letter
 print_header
 print(students)
 print_footer(students)
