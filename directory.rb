9fa82625ab8186b1bb0ae6724dc679c3ac5c1b6a
lsewilson <laurawilson@hotmail.co.uk>
2016-08-02 10:48:46 +0100

    Updated specific letter method and added <12 characters method

--- directory.rb
+++ directory.rb
@@ -16,10 +16,19 @@ def input_students
   students
 end
 
-def certain_letter
+def certain_letter(students)
   puts "With which letter does the names you wish to search for begin?"
   puts "To search for all names, just hit return twice."
-  @initial = gets.chomp.upcase
+  initial = gets.chomp.upcase
+  initial_students = students.select {|student| student[:name][0] == initial}
+  initial_students != [] ? twelve_chars(initial_students) : twelve_chars(students)
+end
+
+def twelve_chars(students)
+  puts "Would you like to search only for students whose names are shorter than 12 characters? (Y/N)"
+  response = gets.chomp.upcase
+  short_names = students.select {|student| student[:name].length < 12}
+  response == "Y" ? print(short_names) : print(students)
 end
 
 def print_header
@@ -28,22 +37,16 @@ def print_header
 end
 
 def print(students)
+  print_header
   students.each_with_index do |student, index|
-    if student[:name][0] == @initial
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    elsif @initial == "" || @initial == nil?
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students."
 end
 # nothing happens until we call the methods
 students = input_students
-certain_letter
-print_header
-print(students)
+certain_letter(students)
 print_footer(students)
