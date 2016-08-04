82efc4dc852e3e8a5ac195e8747e1b447c7344bb
lsewilson <laurawilson@hotmail.co.uk>
2016-08-04 13:33:05 +0100

    Refactored save and load methods to use code block to open and close files

--- directory.rb
+++ directory.rb
@@ -107,23 +107,23 @@ def print_footer
 end
 
 def save_students(filename)
-  file = File.open(filename, "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  File.open(filename, "w") do |f|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      f.puts csv_line
+    end
   end
-  file.close
   puts "#{filename} saved!"
 end
 
 def load_students(filename)
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    add_info_to_array(name, cohort)
+  File.open(filename, "r") do |f|
+    f.readlines.each do |line|
+      name, cohort = line.chomp.split(',')
+      add_info_to_array(name, cohort)
+    end
   end
-  file.close
   puts "#{filename} loaded!"
 end
 
